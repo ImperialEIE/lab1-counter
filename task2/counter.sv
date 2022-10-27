20ca850fbceacf22f19515e51f5bbe43ff3b6a60
Diegovano <36131808+Diegovano@users.noreply.github.com>
2022-10-27 17:05:14 +0100

    Add .gitignore, removing compiled files, update counter with test yourself challenge.

--- task2/counter.sv
+++ task2/counter.sv
@@ -11,5 +11,6 @@ module counter #(
 always_ff @(posedge clk)
     if (rst) count <= {WIDTH{1'b0}};
     // value of count is set to value of count + en, which if 0 will not change anything, otherwise be 1, incremeneting, as required.
-    else     count <= count + {{WIDTH-1{1'b0}}, en};
+    // else     count <= count + {{WIDTH-1{1'b0}}, en};
+    else     count <= count + (en > 0 ? {WIDTH{1'b1}} : -{WIDTH{1'b1}});
 endmodule
--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -20,7 +20,7 @@ int main(int argc, char **argv, char **env) {
     if (vbdOpen() != 1) return (-1);
     vbdHeader("Lab 1: Counter");
 
-    usleep(10e5);
+    // usleep(10e5);
 
     // initialise simulation inputs
     top->clk = 1;
@@ -28,7 +28,7 @@ int main(int argc, char **argv, char **env) {
     top->en = 0;
 
     // run simulation for many clock cycles
-    for (i = 0; i < 300; i++) {
+    for (i = 0; i < 1000; i++) {
 
         // dump variables into VCD file and toggle clock
         for (clk = 0; clk < 2; clk++) {
@@ -38,10 +38,11 @@ int main(int argc, char **argv, char **env) {
         }
 
         // ++++ Send count value to Vbuddy
-        vbdHex(4, (int(top->count) >> 16) & 0xF);
-        vbdHex(3, (int(top->count) >> 8) & 0xF);
-        vbdHex(2, (int(top->count) >> 4) & 0xF);
-        vbdHex(1, int(top->count) & 0xF);
+        // vbdHex(4, (int(top->count) >> 16) & 0xF);
+        // vbdHex(3, (int(top->count) >> 8) & 0xF);
+        // vbdHex(2, (int(top->count) >> 4) & 0xF);
+        // vbdHex(1, int(top->count) & 0xF);
+        vbdPlot(int(top->count), 0, 255);
         vbdCycle(i + 1);
         // ---- end of Vbuddy output section
 
